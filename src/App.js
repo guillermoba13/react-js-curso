b278e5b19680cd072bef1f5a47b580e31e38e5b4
Guillermo <guillermoba9>
2020-02-11 10:19:39 -0600

    Hasta este punto el proyecto corre perfectamente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import tasks from './samples/tasks.json';
+import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
+import Task from './components/Task';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state ={
+    tasks: tasks
+  }
+  addTask = (title, description) =>{
+    const newTask={
+      title: title,
+      description: description,
+      id: this.state.tasks.length
+    }
+    this.setState({tasks: [...this.state.tasks, newTask]})
+  }
+
+  deleteTask = (id) =>{
+    const newTask= this.state.tasks.filter(task => task.id !== id);
+    this.setState({tasks: newTask})
+  }
+
+  checkDone = (id) =>{
+    const newTasks = this.state.tasks.map(task => {
+      if(task.id === id){
+        task.done = !task.done
+      }
+      return task;
+    })
+    this.setState({tasks: newTasks})
+  }
+
+  render(){
+    return(
+      <div>
+        <TaskForm addTask={this.addTask} /> 
+        <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask} checkDone={this.checkDone}/>
+      </div>
+    );
+  }
 }
 
 export default App;
